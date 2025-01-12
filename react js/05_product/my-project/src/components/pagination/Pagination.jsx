c9a172b8fa5a69907b50c7ee166195d26775fbe0
ayaniegain <ayaniegain@gmail.com>
2025-01-12 13:02:47 +0530

    feat: enhance pagination component styles and remove unused ProductDetails component

--- react js/05_product/my-project/src/components/pagination/Pagination.jsx
+++ react js/05_product/my-project/src/components/pagination/Pagination.jsx
@@ -2,17 +2,33 @@ import React from 'react'
 
 function Pagination() {
   return (
-    <div className="join py-2 ">
-    <input
-      className="join-item btn btn-square bg-white  "
-      type="radio"
-      name="options"
-      aria-label="1"
-      defaultChecked />
-    <input className="join-item btn  bg-white btn-square " type="radio" name="options" aria-label="2" />
-    <input className="join-item btn bg-white  btn-square" type="radio" name="options" aria-label="3" />
-    <input className="join-item btn bg-white  btn-square" type="radio" name="options" aria-label="4" />
-  </div>
+    <div className="join py-2">
+      <input
+        className="join-item btn btn-square bg-white text-gray-700 hover:bg-gray-200 focus:bg-blue-500 focus:text-white"
+        type="radio"
+        name="options"
+        aria-label="1"
+        defaultChecked
+      />
+      <input
+        className="join-item btn btn-square bg-white text-gray-700 hover:bg-gray-200 focus:bg-blue-500 focus:text-white"
+        type="radio"
+        name="options"
+        aria-label="2"
+      />
+      <input
+        className="join-item btn btn-square bg-white text-gray-700 hover:bg-gray-200 focus:bg-blue-500 focus:text-white"
+        type="radio"
+        name="options"
+        aria-label="3"
+      />
+      <input
+        className="join-item btn btn-square bg-white text-gray-700 hover:bg-gray-200 focus:bg-blue-500 focus:text-white"
+        type="radio"
+        name="options"
+        aria-label="4"
+      />
+    </div>
   )
 }
 
