d12c73e905ad89a4c0c46e2030f61cb337873751
ayaniegain <ayaniegain@gmail.com>
2025-04-19 09:27:26 +0530

    feat: Initialize React project with Vite, Tailwind CSS, and essential components
    - Added package.json with dependencies for React, ReactDOM, Tailwind CSS, and Vite.
    - Created initial SVG assets for Vite and React logos.
    - Implemented main App component structure with routing and state management.
    - Developed multiple components including Book, Counter, DoM, FormState, and Parent-Child interaction.
    - Integrated useState and useEffect hooks for state management and side effects.
    - Established basic styling with Tailwind CSS and custom CSS.
    - Documented key React concepts and hooks in xdocs.md for reference.

---
+++ Interview Prep/JS interview Prep/exam set 1/answer-set-arpan2.js
@@ -0,0 +1,54 @@
+// 1
+
+// function sumofArray(arr) {
+//   return arr.reduce((a, b) => a + b, 0);
+// }
+
+// let res = sumofArray([1, 2, 3, 4, 5]);
+// console.log(res);
+
+// 2
+// function largesmall(arr) {
+
+//     return arr.reduce((a,b)=>a>b?a:b)
+// }
+
+// let res = largesmall([10, 20, 5, 40, 30]);
+// console.log(res);
+
+// 3
+
+// function revArray(array) {
+//   let rev = [];
+//   for (let i = array.length - 1; i >= 0; i--) {
+//     rev.push(array[i]);
+//   }
+//   return rev;
+// }
+
+// let res = revArray([10, 20, 5, 40, 30]);
+// console.log(res);
+
+// 4
+
+
+
+// function removeElm(array) {
+
+//     return array.filter((elm,i)=>array.indexOf(elm)==i);
+ 
+// }
+
+// let res = removeElm([1, 2, 2, 3, 4, 4, 5]);
+// console.log(res);
+// 6
+
+// function checkPossitive(arr){
+
+//     return arr.filter((elm)=>elm%2==0)
+
+// }
+
+// let res= checkPossitive([1, 2, 3, 4, 5])
+
+// console.log(res)
--- Interview Prep/react js interview Prep/practice-react5-memo-callback/README.md
+++
@@ -1,3 +0,0 @@
-
-
-- *USEMEMO*- It is a hook used to memoize the result of a function and cache it, recalulating it only if it dependencies change.
